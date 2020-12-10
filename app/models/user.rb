92844afc84c893a8295d1f51e8fd44f60ec84ab2
asaPhone2 <amge.hero23@gmail.com>
2020-12-10 13:57:26 +0900

    ルーティング、コントローラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, presence: true,uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :nickname, presence: true
+  validates :password,presence: true, length: { minimum: 6 }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :first_name,presence: true,format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :family_name,presence: true,format: { with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name_kana,presence: true,format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :family_name_kana,presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day,presence: true
 end
--- db/migrate/20201201103426_devise_create_users.rb
+++ db/migrate/20201201103426_devise_create_users.rb
@@ -4,8 +4,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,null: false
+      t.string :first_name,null: false
+      t.string :family_name,null: false
+      t.string :first_name_kana,null: false
+      t.string :family_name_kana,null: false
+      t.date :birth_day,null: false
+      t.string :email,null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -41,4 +48,5 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
+
 end
