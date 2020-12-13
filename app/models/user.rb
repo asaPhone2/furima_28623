ad56ecbe45053a95666ada9f94befb55f4a5b9e0
asaPhone2 <amge.hero23@gmail.com>
2020-12-13 20:24:08 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  with_options presence: true do
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :nickname, presence: true
 
-    with_options format: { with: /^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}$/ } do
-      validates :password, presence: true
       validates :password_confirmation, presence: true
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+   
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i } do
+      validates :password
+      validates :password_confirmation
     end
 
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-      validates :first_name, presence: true
-      validates :family_name, presence: true
-    end
+    with_options presence: true do
+      validates :nickname
 
-    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
-      validates :first_name_kana, presence: true
-      validates :family_name_kana, presence: true
-    end
+      with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+        validates :first_name
+        validates :family_name
+      end
 
-    validates :birth_day, presence: true
+      with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+        validates :first_name_kana
+        validates :family_name_kana
+      end
+
+      validates :birth_day
+    end
 end
