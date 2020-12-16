f2673834c1ed7701cd2d5e356f0ca0d9e2427e36
asaPhone2 <amge.hero23@gmail.com>
2020-12-16 12:04:04 +0900

    バリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,18 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  validates :name, presence:true
+  validates :introduction, presence:true
+  validates :price, presence:true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ }
+  validates :category_id, presence:true
+  validates :item_condition_id, presence:true
+  validates :delivery_fee_id, presence:true
+  validates :prefecture_code_id, presence:true
+  validates :preparation_day_id presence:true
+
+  validates_associated :item_imgs
+  validates :item_imgs, presence:true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :delivery_fee
