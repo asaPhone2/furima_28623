5b7bce7f7fe898fa14854e162266bfb9eb2d3527
asaPhone2 <amge.hero23@gmail.com>
2020-12-20 18:54:02 +0900

    修正4

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -75,6 +75,24 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
+      it 'priceが全角文字では登録できない' do
+        @item.price = 123
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+
+      it 'priceが英数字混合では登録できない' do
+        @item.price = "1a23"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'priceが半角英語だけでは登録できない' do
+        @item.price = "aaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
       it "ユーザーが紐付いていないと出品できない" do
         @item.user = nil
         @item.valid?
