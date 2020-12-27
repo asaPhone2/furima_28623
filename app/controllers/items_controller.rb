1981e993340a199c4906e0566eb6d79fd258f3fb
asaPhone2 <amge.hero23@gmail.com>
2020-12-27 17:08:06 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
+  # before_action :not_user, only: [:edit, :update]
+
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
   end
@@ -18,20 +21,40 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to item_path
+    if @item.user_id == current_user.id
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        render :edit
+      end
+    end
   end
+
+  private
 
   def item_params
     params.require(:item).permit(:introduction, :image, :name, :price, :item_condition_id, :category_id, :delivery_fee_id, :preparation_day_id, :prefecture_code_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def not_user
+    if @item.user_id != current_user.id
+      redirect_to items_path
+    else
+      @item.update(item_params)
+      redirect_to item_path
+    end
   end
 end
